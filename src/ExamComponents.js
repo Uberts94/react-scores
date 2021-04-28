00f62af0220e3dc2c9c0d8172e5dcb3b96cdd696
Luigi De Russis <luigidr@gmail.com>
2021-04-28 22:18:45 +0200

    add client-side routing and edit form

--- src/ExamComponents.js
+++ src/ExamComponents.js
@@ -1,29 +1,17 @@
-import { Col, Table, Form, Button } from 'react-bootstrap';
+import { Col, Table, Form, Button, Alert } from 'react-bootstrap';
 import { useState } from 'react';
 import { iconDelete, iconEdit } from './icons';
 import dayjs from 'dayjs';
+import {Link, Redirect, useLocation} from 'react-router-dom';
 
 
 function ExamScores(props) {
   return <Col>
-    <ExamTable exams={props.exams} courses={props.courses} />
+    <ExamTable exams={props.exams} courses={props.courses} deleteExam={props.deleteExam}/>
   </Col>;
 }
 
 function ExamTable(props) {
-  const [exams, setExams] = useState([...props.exams]);
-  const [showForm, setShowForm] = useState(false);
-
-
-  const deleteExam = (coursecode) => {
-    setExams((exs) => exs.filter(ex => ex.coursecode !== coursecode))
-  }
-
-  const addExam = (exam) => {
-    setExams(oldExams => [...oldExams, exam]);
-}
-
-
 
   return (<>
     <Table striped bordered>
@@ -36,18 +24,17 @@ function ExamTable(props) {
         </tr>
       </thead>
       <tbody>{
-        exams.map((ex) => <ExamRow key={ex.coursecode}
+        props.exams.map((ex) => <ExamRow key={ex.coursecode}
           exam={ex}
           examName={props.courses.filter(c => c.coursecode === ex.coursecode)[0].name}
-          deleteExam={deleteExam}
+          deleteExam={props.deleteExam}
         />)
         /* NOTE: exam={{...ex, name: (the above expression)}} could be a quicker (and dirtier) way to add the .name property to the exam, instead of passing the examName prop */
       }
       </tbody>
     </Table>
-    {showForm ? <ExamForm courses={props.courses}
-      addExam={(exam) => { addExam(exam); setShowForm(false); }}
-      cancel={() => setShowForm(false)} /> : <Button variant='success' onClick={() => setShowForm(true)}>Add</Button>}
+
+    <Link to="/add"><Button variant='success'>Add</Button></Link>
   </>
 
   );
@@ -67,31 +54,54 @@ function ExamRowData(props) {
 
 function RowControls(props) {
   return <td>
-    {iconEdit} <span onClick={() => { props.deleteExam(props.exam.coursecode) }}>{iconDelete}</span>
+    <Link to={{
+      pathname: "/update",
+      state: { exam: props.exam, examDate: props.exam.date.format('YYYY-MM-DD') }
+    }}>{iconEdit}
+  </Link> <span onClick={() => { props.deleteExam(props.exam.coursecode) }}>{iconDelete}</span>
   </td>
 }
 
 function ExamForm(props) {
-  const [course, setCourse] = useState('');
-  const [score, setScore] = useState(30);
-  const [date, setDate] = useState(dayjs());
-
-  const handleSumbit = (event) => {
+  const location = useLocation();
+  const [course, setCourse] = useState(location.state ? location.state.exam.coursecode : '');
+  const [score, setScore] = useState(location.state ? location.state.exam.score : 30);
+  const [date, setDate] = useState(location.state ? location.state.examDate : dayjs().format('YYYY-MM-DD'));
+  const [errorMessage, setErrorMessage] = useState('') ;
+  const [submitted, setSubmitted] = useState(false);
+  
+  const handleSubmit = (event) => {
       event.preventDefault();
-      const exam = { coursecode: course, score: score, date: date };
-      // VALIDATE !!!
-      /* Validation rules: course != '', course not already in props.courses */
-      props.addExam(exam);
+      const exam = { coursecode: course, score: score, date: dayjs(date) };
+      
+      // SOME VALIDATION, ADD MORE!!!
+      let valid = true;
+      if(course === '' || score === '')
+          valid = false;
+      const scorenumber = +score ;
+      if(scorenumber < 18 || scorenumber > 31)
+          valid = false;
+      
+      if(valid)
+      {
+        props.addOrUpdateExam(exam);
+        setSubmitted(true);
+      }
+      else {
+        // show a better error message...
+        setErrorMessage('Error(s) in the form, please fix it.')
+      }
   };
 
   return (
+    <> {submitted ? <Redirect to="/"/> :
       <Form>
+          {errorMessage ? <Alert variant='danger'>{errorMessage}</Alert> : ''}
           <Form.Group controlID='selectedCourse'>
               <Form.Label>Course</Form.Label>
-              <Form.Control as="select" defaultValue='' value={course} onChange={ev => setCourse(ev.target.value)}>
-                  <option disabled hidden value=''>choose...</option>
-                  {props.courses.map(course => <option key={course.coursecode} value={course.coursecode}>{course.name}</option>)}
-                  {/* improvement: don't insert courses already passed */}
+              <Form.Control as="select" value={course} onChange={ev => setCourse(ev.target.value)} disabled={location.state}>
+                <option disabled hidden value=''>choose...</option>
+                {props.courses.map(course => <option key={course.coursecode} value={course.coursecode} >{course.name} </option>)}
               </Form.Control>
           </Form.Group>
           <Form.Group controlID='selectedScore'>
@@ -100,40 +110,12 @@ function ExamForm(props) {
           </Form.Group>
           <Form.Group controlID='selectedDate'>
               <Form.Label>Date</Form.Label>
-              <Form.Control type='date' value={date.format('YYYY-MM-DD')} onChange={ev => setDate(dayjs(ev.target.value))} />
+              <Form.Control type='date' value={date} onChange={ev => setDate(ev.target.value)} />
           </Form.Group>
-          <Button onClick={handleSumbit}>Save</Button> <Button variant='secondary' onClick={props.cancel}>Cancel</Button>
+          <Button onClick={handleSubmit}>Save</Button> <Link to="/"><Button variant='secondary'>Cancel</Button></Link>
       </Form>
-  )
+    }
+    </>)
 }
 
-
-/* ALTERNATIVE IMPLEMENTATION, WITH "NATIVE" HTML CONTROLS (WITHOUT BOOTSTRAP) */
-function ExamForm_native(props) {
-  const [course, setCourse] = useState('');
-  const [score, setScore] = useState(30);
-  const [date, setDate] = useState(dayjs());
-
-  const handleSumbit = (event) => {
-      event.preventDefault();
-      const exam = { coursecode: course, score: score, date: date };
-      props.addExam(exam);
-  };
-
-  return (
-      <form>
-          <span style={{ display: 'inline-block', width: '5em' }}>Course:</span>
-          <select value={course} onChange={ev => setCourse(ev.target.value)}>
-              {props.courses.map(course => <option key={course.coursecode} value={course.coursecode}>{course.name}</option>)}
-          </select><br />
-          <span style={{ display: 'inline-block', width: '5em' }}>Score:</span>
-          <input type='number' min={18} max={31} value={score} onChange={ev => setScore(ev.target.value)} /><br />
-          <span style={{ display: 'inline-block', width: '5em' }}>Date:</span>
-          <input type='date' value={date.format('YYYY-MM-DD')} onChange={ev => setDate(dayjs(ev.target.value))} /><br />
-          <button onClick={handleSumbit}>Save</button> <button onClick={props.cancel}>Cancel</button>
-      </form >
-  )
-}
-
-
-export default ExamScores;
+export {ExamScores, ExamForm};
